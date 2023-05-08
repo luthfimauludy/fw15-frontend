3dc7bbbe2ae9eea5d90de77826fb362ce9238258
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-08 17:56:02 +0700

    feat: booking route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Event from "./pages/Event";
+import Booking from "./pages/Booking";
 import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
-import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
@@ -12,9 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/event" element={<Event />} />
+        <Route path="/booking" element={<Booking />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
