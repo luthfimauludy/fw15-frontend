b3c07e30b9993a30799f883a6eaa7334386b4dc7
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-12 00:16:19 +0700

    feat: set warning message with dispatch

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,17 @@
-// import React from "react";
-import { useSelector } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import propTypes from "prop-types";
+import { setWarningMessage } from "../redux/reducers/auth";
 
 const PrivateRoute = ({ children }) => {
   const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (!token) {
+      dispatch(setWarningMessage("You have to login first"));
+    }
+  });
   if (token) {
     return children;
   } else {
