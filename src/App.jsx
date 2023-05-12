b5eaafe3a81c125b3dc0055b699ff4da554f6733
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-12 15:09:43 +0700

    feat: persistGate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Home from "./pages/Home";
 import DetailEvent from "./pages/DetailEvent";
@@ -21,31 +22,33 @@ import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail-event" element={<DetailEvent />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/change-password" element={<ChangePassword />} />
-          <Route path="/manage-event" element={<ManageEvent />} />
-          <Route path="/my-booking" element={<MyBooking />} />
-          <Route path="/my-wishlist" element={<MyWishlist />} />
-          <Route path="/create-event" element={<CreateEvent />} />
-          <Route path="/update-event" element={<UpdateEvent />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail-event/:id" element={<DetailEvent />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/change-password" element={<ChangePassword />} />
+            <Route path="/manage-event" element={<ManageEvent />} />
+            <Route path="/my-booking" element={<MyBooking />} />
+            <Route path="/my-wishlist" element={<MyWishlist />} />
+            <Route path="/create-event" element={<CreateEvent />} />
+            <Route path="/update-event" element={<UpdateEvent />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 };
