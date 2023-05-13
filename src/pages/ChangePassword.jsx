1ce8be079b6227a894b58148de079455c6aea395
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-14 01:36:09 +0700

    feat: integration profile data with backend

--- src/pages/ChangePassword.jsx
+++ src/pages/ChangePassword.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import http from "../helpers/http";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import {
   FiUser,
@@ -10,303 +14,165 @@ import {
   FiSettings,
   FiLogOut,
 } from "react-icons/fi";
-import { FaWhatsapp, FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
-import wetick from "../assets/images/logo-wetick.png";
-import profile from "../assets/images/profile3.jpg";
+import { useSelector } from "react-redux";
 
 const ChangePassword = () => {
+  const token = useSelector((state) => state.auth.token);
+  const [profile, setProfile] = React.useState({});
+
+  React.useEffect(() => {
+    const getProfile = async () => {
+      const { data } = await http(token).get("/profile");
+      setProfile(data.results);
+    };
+    getProfile();
+  }, []);
+
   return (
     <>
       {/* Navbar Start */}
-      <nav className="flex md:flex-row flex-col md:h-24 px-8 bg-white font-semibold">
-        <div className="flex md:justify-center justify-between items-center">
-          <div className="flex justify-center items-center text-2xl">
-            <img src={wetick} alt="logo" />
-            <div className="text-2xl">
-              <Link to="/">
-                <span className="text-[#61764B]">We</span>
-                <span className="text-[#FF3D71]">tick</span>
-              </Link>
-            </div>
-          </div>
-          <div className="md:hidden flex items-center border rounded-lg p-1 bg-[#61764B]">
-            <button id="btn-toggler">
-              <i className="text-white" data-feather="menu"></i>
-            </button>
-          </div>
-        </div>
-        <div
-          id="menu"
-          className="hidden md:flex md:flex-row flex-col flex-1 md:pl-8 mb-5 md:mb-0 text-sm"
-        >
-          <ul className="flex justify-center md:flex-row flex-col flex-1 gap-3 text-black">
-            <li className="flex justify-center items-center min-w-[100px]">
-              <Link
-                className="hover:text-[#61764B] pb-2.5 border-b border-transparent hover:border-[#61764B]"
-                to="/"
-              >
-                Home
-              </Link>
-            </li>
-            <li className="flex justify-center items-center min-w-[100px]">
-              <Link
-                className="hover:text-[#61764B] pb-2.5 border-b border-transparent hover:border-[#61764B]"
-                to="/create-event"
-              >
-                Create Event
-              </Link>
-            </li>
-            <li className="flex justify-center items-center min-w-[100px]">
-              <Link
-                className="hover:text-[#61764B] pb-2.5 border-b border-transparent hover:border-[#61764B]"
-                to="/event"
-              >
-                Location
-              </Link>
-            </li>
-          </ul>
-          <div className="flex justify-center gap-3 items-center text-sm font-semibold">
-            <div className="inline-block rounded-full p-[2px] bg-gradient-to-r from-[#61764B] to-[#A0D995]">
-              <img
-                className="w-11 h-11 object-cover rounded-full border-2 border-white"
-                src={profile}
-                alt="Profile"
-              />
-            </div>
-            <div className="text-black">
-              <Link to="/profile">John Tomson</Link>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Header />
       {/* Navbar End */}
       {/* Main Content Start */}
-      <main className="flex mt-5 md:mt-0 md:pt-12 pb-24 md:px-16 bg-white md:bg-[#E9EDC9]">
-        {/* Left Content Start */}
-        <aside className="hidden md:inline mr-16">
-          <div className="flex gap-3.5 mb-14">
-            <div className="flex gap-3.5">
-              <div className="inline-block rounded-full p-[2px] bg-gradient-to-r from-[#61764B] to-[#A0D995]">
-                <img
-                  className="w-11 h-11 object-cover rounded-full border-2 border-white"
-                  src={profile}
-                  alt="Profile"
+      <main className="md:bg-[#E9EDC9] md:pt-12">
+        <div className="flex mt-5 md:mt-0 mb-24 md:mx-16">
+          {/* Left Content Start */}
+          <aside className="hidden md:inline pr-16 md:bg-[#E9EDC9]">
+            <div className="flex gap-3.5 mb-14">
+              <div className="flex gap-3.5">
+                <div className="inline-block rounded-full p-[2px] bg-gradient-to-r from-[#61764B] to-[#A0D995]">
+                  {profile?.picture && (
+                    <img
+                      className="w-11 h-11 object-cover rounded-full border-2 border-white"
+                      src={`http://localhost:8888/uploads/${profile?.picture}`}
+                      alt="Profile"
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col justify-between text-black">
+                <div className="text-sm font-semibold">{profile?.fullName}</div>
+                <div className="text-xs">Entrepreneur, ID</div>
+              </div>
+            </div>
+            <ul className="text-sm text-black font-semibold">
+              <li className="flex gap-4 mb-8">
+                <FiUser size={25} color="#C1C5D0" />
+                Profile
+              </li>
+              <li>
+                <ul className="ml-12">
+                  <li className="flex gap-4 mb-8 relative">
+                    <FiCreditCard size={25} color="#C1C5D0" />
+                    Card
+                    <div className="absolute w-28 border border-black top-2"></div>
+                  </li>
+                  <li>
+                    <Link className="flex gap-4 mb-8" to="/profile">
+                      <FiEdit3 size={25} color="#C1C5D0" />
+                      Edit Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className="flex gap-4 mb-8 text-primary"
+                      to="/change-password"
+                    >
+                      <FiLock size={25} />
+                      Change Password
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link className="flex gap-4 mb-8" to="/manage-event">
+                  <FiPlusCircle size={25} color="#C1C5D0" />
+                  Create Event
+                </Link>
+              </li>
+              <li>
+                <Link className="flex gap-4 mb-8" to="/my-booking">
+                  <FiList size={25} color="#C1C5D0" />
+                  My Booking
+                </Link>
+              </li>
+              <li>
+                <Link className="flex gap-4 mb-8" to="/my-wishlist">
+                  <FiHeart size={25} color="#C1C5D0" />
+                  My Wishlist
+                </Link>
+              </li>
+              <li>
+                <Link className="flex gap-4 mb-8" to="">
+                  <FiSettings size={25} color="#C1C5D0" />
+                  Settings
+                </Link>
+              </li>
+              <li>
+                <Link className="flex gap-4 mb-8" to="/">
+                  <FiLogOut size={25} color="#F03800" />
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </aside>
+          {/* Left Content End */}
+          {/* Right Content Start */}
+          <div className="flex-1 bg-white text-black md:min-h-[625px] md:px-12 md:pt-11 rounded-2xl">
+            <div className="text-xl font-semibold tracking-wide mb-12">
+              Change Password
+            </div>
+            <form className="flex flex-col w-full gap-6 md:gap-8">
+              <div className="flex flex-col md:flex-row items-start md:items-center">
+                <label className="md:w-48 w-full mb-2.5 md:mb-0">
+                  Old Password
+                </label>
+                <input
+                  className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
+                  type="password"
+                  name="old-password"
+                  placeholder="Input Here"
                 />
               </div>
-            </div>
-            <div className="flex flex-col justify-between text-black">
-              <div className="text-sm font-semibold">Jhon Tomson</div>
-              <div className="text-xs">Entrepreneur, ID</div>
-            </div>
+              <div className="flex flex-col md:flex-row items-start md:items-center">
+                <label className="md:w-48 w-full mb-2.5 md:mb-0">
+                  New Password
+                </label>
+                <input
+                  className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
+                  type="password"
+                  name="new-password"
+                  placeholder="Input Here"
+                />
+              </div>
+              <div className="flex flex-col md:flex-row items-start md:items-center">
+                <label className="md:w-48 w-full mb-2.5 md:mb-0">
+                  Confirm Password
+                </label>
+                <input
+                  className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
+                  type="password"
+                  name="confirm-password"
+                  placeholder="Input Here"
+                />
+              </div>
+              <div>
+                <button
+                  className="bg-[#61764b] h-14 w-full rounded-2xl shadow-lg shadow-[#61764B] text-white"
+                  type="button"
+                >
+                  Update
+                </button>
+              </div>
+            </form>
           </div>
-          <ul className="text-sm text-black font-semibold">
-            <li className="flex gap-7 mb-8">
-              <FiUser size={25} color="#C1C5D0" />
-              Profile
-            </li>
-            <li>
-              <ul className="ml-12">
-                <li className="flex gap-7 mb-8 relative">
-                  <FiCreditCard size={25} color="#C1C5D0" />
-                  Card
-                  <div className="absolute w-28 border border-black top-2"></div>
-                </li>
-                <li>
-                  <Link className="flex gap-7 mb-8" to="/profile">
-                    <FiEdit3 size={25} color="#C1C5D0" />
-                    Edit Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="flex gap-7 mb-8 text-primary"
-                    to="/change-password"
-                  >
-                    <FiLock size={25} />
-                    Change Password
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link className="flex gap-7 mb-8" to="/manage-event">
-                <FiPlusCircle size={25} color="#C1C5D0" />
-                Create Event
-              </Link>
-            </li>
-            <li>
-              <Link className="flex gap-7 mb-8" to="/my-booking">
-                <FiList size={25} color="#C1C5D0" />
-                My Booking
-              </Link>
-            </li>
-            <li>
-              <Link className="flex gap-7 mb-8" to="/my-wishlist">
-                <FiHeart size={25} color="#C1C5D0" />
-                My Wishlist
-              </Link>
-            </li>
-            <li>
-              <Link className="flex gap-7 mb-8" to="">
-                <FiSettings size={25} color="#C1C5D0" />
-                Settings
-              </Link>
-            </li>
-            <li>
-              <Link className="flex gap-7 mb-8" to="/">
-                <FiLogOut size={25} color="#F03800" />
-                Logout
-              </Link>
-            </li>
-          </ul>
-        </aside>
-        {/* Left Content End */}
-        {/* Right Content Start */}
-        <div className="flex-1 bg-white text-black md:min-h-[825px] md:px-12 md:pt-11 rounded-2xl">
-          <div className="text-xl font-semibold tracking-wide mb-12">
-            Change Password
-          </div>
-          <form className="flex flex-col w-full gap-6 md:gap-8">
-            <div className="flex flex-col md:flex-row items-start md:items-center">
-              <label className="md:w-48 w-full mb-2.5 md:mb-0">
-                Old Password
-              </label>
-              <input
-                className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
-                type="password"
-                name="old-password"
-                placeholder="Input Here"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row items-start md:items-center">
-              <label className="md:w-48 w-full mb-2.5 md:mb-0">
-                New Password
-              </label>
-              <input
-                className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
-                type="password"
-                name="new-password"
-                placeholder="Input Here"
-              />
-            </div>
-            <div className="flex flex-col md:flex-row items-start md:items-center">
-              <label className="md:w-48 w-full mb-2.5 md:mb-0">
-                Confirm Password
-              </label>
-              <input
-                className="w-full md:flex-1 h-14 px-7 outline-none border border-[#c1c5d0] rounded-lg"
-                type="password"
-                name="confirm-password"
-                placeholder="Input Here"
-              />
-            </div>
-            <div>
-              <button
-                className="bg-[#61764b] h-14 w-full rounded-2xl shadow-lg shadow-[#61764B] text-white"
-                type="button"
-              >
-                Update
-              </button>
-            </div>
-          </form>
+          {/* Right Content End */}
         </div>
-        {/* Right Content End */}
+        {/* Main Content End */}
+        {/* Footer Start */}
+        <Footer />
+        {/* Footer End */}
       </main>
-      {/* Main Content End */}
-      {/* Footer Start */}
-      <footer>
-        <div className="flex flex-col md:items-center p-2.5 md:p-0 gap-12 md:gap-20 bg-[#E9EDC9]">
-          <div className="flex md:flex-row flex-col md:justify-center gap-14 md:gap-32">
-            <div>
-              <div className="flex items-center text-2xl mb-4 md:mb-8 font-semibold">
-                <img src={wetick} alt="logo" />
-                <a href="/index.html">
-                  <span className="text-[#61764B]">We</span>
-                  <span className="text-[#FF3D71]">tick</span>
-                </a>
-              </div>
-              <div className="text-sm text-black mb-4">
-                Find events you love with our
-              </div>
-              <div className="flex gap-4 text-[#C1C5D0]">
-                <a href="https://www.facebook.com">
-                  <FaFacebook />
-                </a>
-                <a href="https://www.whatsapp.com">
-                  <FaWhatsapp />
-                </a>
-                <a href="https://www.instagram.com">
-                  <FaInstagram />
-                </a>
-                <a href="https://www.twitter.com">
-                  <FaTwitter />
-                </a>
-              </div>
-            </div>
-            <div>
-              <ul className="flex flex-col gap-4">
-                <li className="text-black font-semibold">Wetick</li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">About Us</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Features</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Blog</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Payments</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Mobile App</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <ul className="flex flex-col gap-4">
-                <li className="text-black font-semibold">Features</li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Booking</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Create Event</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Discover</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Register</a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <ul className="flex flex-col gap-4">
-                <li className="text-black font-semibold">Company</li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Partnership</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Help</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Term of Service</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Privacy Policy</a>
-                </li>
-                <li className="text-sm text-[#C1C5D0]">
-                  <a href="#">Sitemap</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <p className="text-base font-semibold text-[#5A7184] md:w-[860px]">
-            © 2020 Wetick All Rights Reserved
-          </p>
-        </div>
-      </footer>
-      {/* Footer End */}
     </>
   );
 };
